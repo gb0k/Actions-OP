1e5f22552ad78165f88dc454134223a23e1f9bd1
Dickson Lee <30920372+gb0k@users.noreply.github.com>
2020-05-19 11:05:22 +0800

    add something

--- third_party.sh
+++ third_party.sh
@@ -7,3 +7,4 @@
 cd openwrt
 
 sed -i '$a\src-git rosy https://github.com/rosywrt/luci-theme-rosy' feeds.conf.default
+sed -i '$a\src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
